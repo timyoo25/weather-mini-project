7cd0ac16a8296491b07898b0c7e40dfbea1f59e7
Tim <timyoo25@gmail.com>
2022-01-03 11:07:54 -0700

    select measurements

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const apiKey = '1af07cb078ea98c3b4b4661a1ba6f347'
 const wURL = 'https://api.openweathermap.org/data/2.5/weather?'
+
+const weather = document.querySelector('#weather-data')
+const city = document.querySelector('#select-city')
+const search = document.querySelector('button')
